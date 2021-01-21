491c5ecd814584a0f7481d53d423eabb330b286c
jonathan <yonibrese@gmail.com>
2021-01-21 13:52:38 +0200

    restructed and styled the homepage and main menu

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './menu-item.styles.scss';
+
+const MenuItem = ({title, imageUrl, size}) => (
+    <div className={`menu-item ${size}`} style={{
+        backgroundImage: `url(${imageUrl})`
+    }}>
+        <div className="content">
+            <h1 className="title">{title}</h1>
+            <span className="subtitle">Shop Now</span>
+        </div>
+    </div>
+    
+);
+
+export default MenuItem;
