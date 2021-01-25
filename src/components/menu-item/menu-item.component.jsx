0d268c30ff2773af29ef30bd261fca82e81634ae
jonathan <yonibrese@gmail.com>
2021-01-25 14:24:17 +0200

    styled menu items and finished lesson 70 on udemy

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 import './menu-item.styles.scss';
 
 const MenuItem = ({title, imageUrl, size}) => (
-    <div className={`menu-item ${size}`} style={{
-        backgroundImage: `url(${imageUrl})`
-    }}>
+    <div className={`menu-item ${size}`} >
+        <div className="backgroundImage" style={{
+            backgroundImage: `url(${imageUrl})`
+        }}/>
         <div className="content">
-            <h1 className="title">{title}</h1>
+            <h1 className="title">{title.toUpperCase()}</h1>
             <span className="subtitle">Shop Now</span>
         </div>
     </div>
