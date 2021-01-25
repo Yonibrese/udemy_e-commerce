6e2d92ef7d9ef5bd032bcf8cb10258b413e5a5c8
jonathan <yonibrese@gmail.com>
2021-01-25 16:04:55 +0200

    basic routing done and finished udemy lesson 77

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
 
+import { Switch, Route } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component'
+
+const HatPage = () => (
+  <div>
+  <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop/hats' component={HatPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import { withRouter } from 'react-router-dom';
+
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl, size}) => (
-    <div className={`menu-item ${size}`} >
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
+    <div className={`menu-item ${size}`} 
+    onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div className="backgroundImage" style={{
             backgroundImage: `url(${imageUrl})`
         }}/>
@@ -15,4 +18,4 @@ const MenuItem = ({title, imageUrl, size}) => (
     
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
