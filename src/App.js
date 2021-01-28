6665eba4cf98c02ec63542e34c4f0e696036382a
jonathan <yonibrese@gmail.com>
2021-01-28 12:59:05 +0200

    page header/navbar componenet

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import { Switch, Route } from 'react-router-dom';
 
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.componet';
 
@@ -10,6 +11,7 @@ import ShopPage from './pages/shop/shop.componet';
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
