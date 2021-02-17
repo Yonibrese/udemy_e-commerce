323551dacb3587d44405183eb49b6084bf8f54c1
jonathan <yonibrese@gmail.com>
2021-02-17 14:37:32 +0200

    creaded removeSingle item and remove all of this item from cart actions. attached it to the cart item component

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import cartActionTypes from './cart.types';
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, removeItemFormCart } from './cart.utils';
 
 const INITIAL_STATE = {
     hidden: true,
@@ -18,6 +18,17 @@ const cartReducer = (state=INITIAL_STATE, action) => {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload)
             };
+        case cartActionTypes.REMOVE_ITEM:
+            return{
+                ...state, 
+                cartItems: removeItemFormCart(state.cartItems, action.payload)
+            }
+
+        case cartActionTypes.CLEAR_ITEM_FROM_CART:
+            return{
+                ...state,
+                cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
+            }
         default:
             return state;
     }
