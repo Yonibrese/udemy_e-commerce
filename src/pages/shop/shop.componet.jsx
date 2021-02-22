86a23bc1cbfdc7f53e9ec45a546ff20f1280394c
jonathan <yonibrese@gmail.com>
2021-02-22 09:26:33 +0200

    moved shopdata into it's own reducer, created shop-collections overview component in preperation for adv routing

--- src/pages/shop/shop.componet.jsx
+++ src/pages/shop/shop.componet.jsx
@@ -1,30 +1,14 @@
 import React from 'react';
 
-import SHOP_DATA from './shop.data.js';
 
-import  CollectionPreview  from '../../components/collection-preview/collection-preview.component.jsx';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-class ShopPage extends React.Component {
-    constructor(props){
-        super(props);
-        
-        this.state={
-            collections: SHOP_DATA
-        }
-    }
+const ShopPage = ({ collections }) => (
+    <div className="shop-page">
+        <CollectionsOverview />
+    </div>
+);
 
-    render(){
-        const {collections} = this.state;
-        return(
-            <div className="shop-page">
-                {
-                    collections.map(({id, ...otherCollectionProps}) =>(
-                        <CollectionPreview key={id} {...otherCollectionProps} />
-                    ))
-                }
-            </div>
-        ); 
-    }
-}
+
 
 export default ShopPage;
